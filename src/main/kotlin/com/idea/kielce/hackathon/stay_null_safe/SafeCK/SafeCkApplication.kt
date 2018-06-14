0df63390d6c287b3dbd55c179745c3ee2304eafa
Damian-Lenovo <333damian333@gmail.com>
2018-06-14 18:57:12 +0200

    Initialize Springboot

---
+++ src/main/kotlin/com/idea/kielce/hackathon/stay_null_safe/SafeCK/SafeCkApplication.kt
@@ -0,0 +1,11 @@
+package com.idea.kielce.hackathon.stay_null_safe.SafeCK
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SafeCkApplication
+
+fun main(args: Array<String>) {
+    runApplication<SafeCkApplication>(*args)
+}
