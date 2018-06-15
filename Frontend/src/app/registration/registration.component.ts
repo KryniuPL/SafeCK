606f9a172c7bc5f2b7c72ad3e610aad0030c1fa7
KryniuPL <krzysztof.draganfm@gmail.com>
2018-06-15 17:15:14 +0200

    Frontend part 12

--- Frontend/src/app/registration/registration.component.ts
+++ Frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+
 
 @Component({
   selector: "app-registration",
@@ -8,11 +18,34 @@ import { Component, OnInit } from "@angular/core";
 
 export class RegistrationComponent implements OnInit {
   
+  myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  username: FormControl;
+  password: FormControl;
+
   constructor() { 
 
   }
 
   ngOnInit() {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+          username: new FormControl('',Validators.required),
+          firstName: new FormControl('', Validators.required), 
+          lastName: new FormControl('', Validators.required),
+      }),
+      email: new FormControl('', [ 
+          Validators.required,
+          Validators.pattern("[^ @]*@[^ @]*") 
+      ]),
+      password: new FormControl('', [
+          Validators.minLength(8), 
+          Validators.required
+      ]),
+      language: new FormControl() 
+  });
+}
 
-  }
 }
