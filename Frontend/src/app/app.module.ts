3844582eae4bfcc8bdfa5cec6c07f16c9edca940
KryniuPL <krzysztof.draganfm@gmail.com>
2018-06-15 13:30:46 +0200

    Frontene with bootstrap configured

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component'
+import { NavbarComponent } from '../app/navbar/navbar.component'
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: '', component : HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
