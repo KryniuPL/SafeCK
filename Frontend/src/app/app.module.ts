7f4cd6f8817f1fb6733971f82a7798be87d5aa15
KryniuPL <krzysztof.draganfm@gmail.com>
2018-06-15 14:03:31 +0200

    Login and Register component added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component'
+import { LoginComponent } from '../app/login/login.component'
+import { RegistrationComponent } from '../app/registration/registration.component'
 import { NavbarComponent } from '../app/navbar/navbar.component'
 import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
-  { path: '', component : HomeComponent}
+  { path: '', component : HomeComponent},
+  { path: 'login', component : LoginComponent},
+  { path: 'register', component : RegistrationComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     RouterModule.forRoot(
